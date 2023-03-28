181e2dfbdbc7d94cea37950625e2e56e4eb216ca
Pinidu Maneesha <pmjuniorcomtec@gmail.com>
2023-03-28 22:28:43 +0530

    up

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link rel="icon" type="image/x-icon" href="/assets/favicon.ico" />
+        {/* Global site tag (gtag.js) - Google Analytics*/}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0LMS757CZR"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          
+          gtag('config', 'G-0LMS757CZR');
+          `}
+          <link rel="icon" type="image/x-icon" href="/assets/favicon.ico" />
+        </Script>
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
